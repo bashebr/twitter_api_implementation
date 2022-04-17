184467f08eefc7a681c2dee73ed7a0d3fe5e199d
bashebr <bgashebr@gmail.com>
2022-04-18 01:37:31 +0200

    feat: add capability to get input from commandline

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
 from download_tweets.download_tweets import get_all_tweets
 
 if __name__ == "__main__":
-    # pass in the username of the account you want to download
-    tweeter_name = input("Enter the tweeter name: ")
-    get_all_tweets(tweeter_name)
+    if len(sys.argv) == 2:
+        get_all_tweets(sys.argv[1])
+    else:
+        print("Usage: python main.py <screen_name>")
